a5969962b30907e5f371b509890b9339815be1dd
Giannoulo <giannoulopoulosg@gmail.com>
2022-04-08 12:46:16 +0300

    Testing vibration

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -96,6 +96,7 @@ const Dashboard = ({
     const winner = getWinner(tileArray);
     if (winner !== "none") {
       setGameOver(winner);
+      navigator.vibrate([100]);
     }
   }, [tileArray]);
 
